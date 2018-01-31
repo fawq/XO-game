02419e6a59c80aad61ab39a54c7eac9187c281bc
zurekpiotr <zurekpiotr50@gmail.com>
2018-01-31 18:48:19 +0100

    conection error

--- src/HibernateUtil.java
+++ src/HibernateUtil.java
@@ -3,6 +3,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
+
  
 public class HibernateUtil {
  

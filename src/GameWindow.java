0940acacc9cfb1957c604f2fdf16409ac44ba0bc
zurekpiotr <zurekpiotr50@gmail.com>
2018-01-31 14:16:10 +0100

    GameWindow

---
+++ src/GameWindow.java
@@ -0,0 +1,12 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class GameWindow extends JFrame{
+
+	JButton buttonTab[][] = new JButton[3][3];
+	
+	public GameWindow() {
+		
+			
+	}
+}
